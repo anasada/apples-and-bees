157993f4a727858ffd71b9544e6f8e543e4dafba
anasada <122490591+anasada@users.noreply.github.com>
2023-10-11 15:28:38 -0700

    Updated connect.php

--- connect.php
+++ connect.php
@@ -1,12 +1,20 @@
 <?php 
 
-$username=$_POST['username'];
-$map_num=$_POST['map_num'];
-$r_time=$_POST['r_time'];
-$points_to=$_POST['signal'];
-$help_rt=$_POST['help_rt'];
-$rec_path=$_POST['rec_path'];
-$rec_score=$_POST['rec_score'];
+
+$username="test";
+$map_num=1;
+$config="AAB";
+$point_to=2;
+$conf_rating=3;
+$attention_rt=4;
+
+// $username=$_POST['username'];
+// $map_num=$_POST['map_num'];
+// $config=$_POST['config'];
+// $point_to=$_POST['signal'];
+// $conf_rating=$_POST['conf_rating'];
+// $attention_rt=$_POST['attention_rt'];
+
 
 $dbhost='co28d739i4m2sb7j.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
 $dbuser='ciqkb98i04oi5mqj';
@@ -15,8 +23,8 @@ $db='xj2tpr6deo31cluv';
 $con=mysqli_connect($dbhost,$dbuser,$dbpass,$db);
 
 if($con){
-    // echo "connection successful!";
-    $sql="insert into `user_response`(username,map_num,r_time,points_to,help_rt,rec_path,rec_score)values('$username','$map_num','$r_time','$points_to','$help_rt','$rec_path','$rec_score')";
+    echo "connection successful!";
+    $sql="INSERT INTO `user_response`(username,map_num,config,point_to,conf_rating,attention_rt)VALUES('$username','$map_num','$config','$point_to','$conf_rating','$attention_rt')";
     $result=mysqli_query($con,$sql);
 }else{
     die(mysqli_error($con));
